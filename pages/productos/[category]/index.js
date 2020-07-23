1703c273eab8f7aa84de9bc25e2b804a9e708ba7
Bernardo <moonbe77@gmail.com>
2020-07-23 21:41:29 +1000

    routes chnaged products fetch route change

--- pages/productos/[category]/index.js
+++ pages/productos/[category]/index.js
@@ -3,34 +3,27 @@ import { useRouter } from "next/router";
 import { useCategories } from "../../../context/CategoriesProvider";
 import axios from "axios";
 import styled from "styled-components";
-
 import MainLayout from "../../../components/layouts/MainLayout";
 import SideBarMenuClient from "../../../components/SideBarMenuClient";
 import ListProducts from "../../../components/ListProducts";
-
-const fetcher = (...args) =>
-  axios(...args)
-    .then((res) => {
-      return res;
-    })
-    .catch((err) => console.log(err));
 
 const ListSection = styled.section`
   display: flex;
 `;
 
-const ProductsWrapper = ({ params }) => {
+const ProductsWrapper = () => {
   const [products, setProducts] = useState(false);
+  const { categorySelected } = useCategories();
   const Router = useRouter();
-  console.log(Router);
+  const query = Router.query;
 
-  const { categorySelected, handleClickCategory } = useCategories();
 
   const getProducts = async () => {
-    const urlProductsByCategory = `${process.env.NEXT_PUBLIC_API_URL}/api/product/category/${params.cid}`;
+
+    const urlProductsByCategory = `${process.env.NEXT_PUBLIC_API_URL}/api/product/category/${query.cid}`;
+
     await axios(urlProductsByCategory)
       .then((res) => {
-        console.log(res);
         setProducts(res.data);
       })
       .catch((err) => console.log(err));
@@ -38,20 +31,10 @@ const ProductsWrapper = ({ params }) => {
     return;
   };
 
-  const handleRouteChange = async (url) => {
-    console.log("App is changing to: ", Router.query);
-    getProducts();
-  };
-
   useEffect(() => {
-    // handleClickCategory(Router.query.cid); //set category selected on context
     getProducts();
-
-    Router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      Router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, []);
+ 
+  }, [query]);
 
   return (
     <div>
@@ -66,7 +49,3 @@ const ProductsWrapper = ({ params }) => {
 ProductsWrapper.Layout = MainLayout;
 
 export default ProductsWrapper;
-
-export async function getServerSideProps({ params }) {
-  return { props: { params } };
-}
