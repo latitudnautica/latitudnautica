cc49099b7fd9256b2825fd444b42c8c82df905ba
Bernardo Munz <moonbe77@gmail.com>
2020-08-12 18:18:46 +1000

    STYLE: detail page styles and error handeling

--- pages/detalle/[p_name]/[product_id].js
+++ pages/detalle/[p_name]/[product_id].js
@@ -1,6 +1,8 @@
+import Head from "next/head";
 import axiosbase from "utils/axiosBase";
 import styled from "styled-components";
-import MainLayout from "../../../components/layouts/MainLayout";
+import MainLayout from "components/layouts/MainLayout";
+import Error from "next/error";
 
 const ProductStyled = styled.main``;
 
@@ -8,6 +10,9 @@ const ProductWrapper = styled.div`
   margin: auto;
   display: flex;
   max-width: 1000px;
+  border: 1px solid #f0f0f0;
+  padding: 1em;
+  border-radius: 5px;
 
   @media (max-width: 640px) {
     flex-direction: column;
@@ -42,64 +47,81 @@ const ProductInfo = styled.div`
   /* border: 2px solid green; */
   display: flex;
   flex-direction: column;
-  margin: 0 20px;
   flex-basis: 700px;
   flex-shrink: 3;
-
-  div {
-    margin: 1em;
-  }
-
+  background: #f0f0f0;
+  border-top: 4px solid ${({ theme }) => theme.colors.primary};
+  border-bottom: 4px solid ${({ theme }) => theme.colors.primary};
+  padding: 2em;
   @media (max-width: 640px) {
     text-align: center;
   }
 `;
 
-const Title = styled.div`
+const Title = styled.h1`
   font-size: 1.5em;
   font-weight: 700;
+  text-transform: uppercase;
+  margin: 0px;
 `;
-const Price = styled.div``;
+const Brand = styled.h3`
+  margin-top: 5px;
+`;
+const Price = styled.h2`
+  color: ${({ theme }) => theme.colors.price};
+  margin: 0px 0 20px 0;
+`;
 const Description = styled.div``;
 const Code = styled.div``;
 const Iva = styled.div``;
 
-export default function Producto(props) {
+const Producto = ({ errorCode, product }) => {
   const { product } = props;
 
+  if (errorCode) <Error statusCode={errorCode} />;
+
   return (
-    <ProductStyled>
-      <ProductWrapper>
-        <ProductImage>
-          <img
-            src={
-              product.imagePath
-                ? process.env.NEXT_PUBLIC_API_URL + product.imagePath
-                : "/images/logo_test.jpg"
-            }
-          />
-        </ProductImage>
-        <ProductInfo>
-          <Title>{product.name}</Title>
-          <Price> $ {product.price}</Price>
-          <Description>{product.description}</Description>
-          <Code>Código: {product.codeArticle}</Code>
-          <Iva>{product.tasaIVA}</Iva>
-        </ProductInfo>
-      </ProductWrapper>
-    </ProductStyled>
+    <>
+      <Head>
+        <title>{product.name}</title>
+      </Head>
+      <ProductStyled>
+        <ProductWrapper>
+          <ProductImage>
+            <img
+              src={
+                product.imagePath
+                  ? process.env.NEXT_PUBLIC_API_URL + product.imagePath
+                  : "/images/logo_test.jpg"
+              }
+            />
+          </ProductImage>
+          <ProductInfo>
+            <Title>{product.name}</Title>
+            <Brand>{product.brand}</Brand>
+            <Price> $ {product.price}</Price>
+            <Description>{product.description}</Description>
+            <Code>Código: {product.codeArticle}</Code>
+            <Iva>{product.tasaIVA}</Iva>
+          </ProductInfo>
+        </ProductWrapper>
+      </ProductStyled>
+    </>
   );
-}
+};
 
 Producto.Layout = MainLayout;
+
+export default Producto;
 
 export async function getServerSideProps({ params }) {
   const pid = params.product_id;
   const product = await axiosbase(`/product/detail/${pid}`).then(
     (res) => res.data
   );
+  const errorCode = res.ok ? false : res.statusCode;
 
   return {
-    props: { product },
+    props: { errorCode, product },
   };
 }
--- styles/theme.js
+++ styles/theme.js
@@ -1,9 +1,9 @@
 const colors = {
   background: "#f29b0b",
   backgroundHover: "#cd8104",
-  primary: "#4888ca",
+  primary: "#f8d395",
   paleCerulean: "#98C1D9",
-  blueSapphire: "#326273",
+  price: "#326273",
   greenPantone: "#4DAA57",
   celadon: "#3B5DDA4",
   raisinBlack: "#25171A",
@@ -18,7 +18,7 @@ const button = {
 };
 
 const details = {
-  boxShadow: "0 0 8px -4px #25171A",
+  boxShadow: "0 0 5px -4px #25171A",
   boxShadowBottom: "0 10px 18px -10px #25171A"
 };
 
